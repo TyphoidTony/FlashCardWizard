39c55b8f93bbea544108bea2b52dfb0722b7c84e
TyphoidTony <stuthagen@gmail.com>
2015-06-27 08:14:39 -0400

    Made main, thread safe.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
-/*
-This literally does exactly what you think it does.
+import javax.swing.*;
+
+/**
+ * Created on 5/5/2014
+ * Created by Anthony Soto
  */
-
-
 
 public class Main {
     public static void main(String[] args){
-        new FlashOptionsWindow();
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new FlashOptionsWindow();
+            }
+        });
+
     }
 
 }
